27712a7e2a153e7a80e2ed5996481c209a57173d
esma3 <semababayeva9898@gmail.com>
2022-10-31 04:30:16 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+
+function activeChat(){
+    document.getElementById('chatbox').style.right="0px"
+}
+
+
+function Changebg(){
+    document.getElementById('main').style.animationName="go" 
+}
+function StAni(){
+    document.getElementById('main').style.animationName="" 
+}
+
+function ChngeMsr(){
+    document.getElementById('main').style.width="400px"
+}
+function Defaultwidth(){
+    document.getElementById("main").style.width="100px"
+}
