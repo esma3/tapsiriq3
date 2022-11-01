35914cdaaa2d5ca30ca980fd1fc946451681316c
esma3 <semababayeva9898@gmail.com>
2022-11-02 02:56:04 +0400

    second commit

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@
 function activeChat(){
     document.getElementById('chatbox').style.right="0px"
 }
+function deactivateChat(){
+    document.getElementById('chatbox').style.right="-400px"
+}
+
 
 
 function Changebg(){
@@ -13,8 +17,17 @@ function StAni(){
 }
 
 function ChngeMsr(){
-    document.getElementById('main').style.width="400px"
+    var eni= document.getElementById('width').value
+    var olchu= document.getElementById('olchu').value
+    document.getElementById('main').style.width=eni+olchu
 }
 function Defaultwidth(){
     document.getElementById("main").style.width="100px"
 }
+
+
+
+function GeceGunduz() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
