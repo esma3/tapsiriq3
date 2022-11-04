beace869fb8f355f802e1ac1b4268f2bb59008ed
esma3 <semababayeva9898@gmail.com>
2022-11-05 00:03:45 +0400

    second commit

--- js/about.js
+++ js/about.js
@@ -1,4 +1,77 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
+// const btn = document.getElementById('btn');
+
+// btn.addEventListener('click', function onChange(event) {
+//   // 👇️ change background color
+//   document.body.style.backgroundColor = 'salmon';
+
+//   // 👇️ optionally change text color
+//   // document.body.style.color = 'white';
+// });
+
+
+// function rengiSec(){
+//   var select =document.getElementById('select')
+//   document.body.style.backgroundColor =select
+// }
+
+
+
+// function rengiSec(){
+//   var select =document.getElementById('select')
+//   document.body.style.backgroundColor =select
+// }
+
+
+// var select = document.querySelector("#my-select");
+// var body = document.body;
+// select.addEventListener('change', function() {
+//   var options = this.options;
+//   var selectedOption = options[options.selectedIndex];
+//   body.style.background = selectedOption.value;
+
+// });
+
+
+
+
+// function sema() {
+//   myimage.src = ColorSelect.children[ColorSelect.selectedIndex].getAttribute('value');
+// }
+
+
+
+// function sema(){
+//   var select= document.getElementById('select')
+//   var sekil= document.getElementById('sekil')
+
+//   sekil.src="images/"+select.value+".jpg"
+// }
+
+
+
+function changeColor(){
+  let color = document.getElementById('colorInputColor').value;
+  document.body.style.backgroundColor =color;
+  document.getElementById('colorInputText').value=color;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -21,6 +21,34 @@ function ChngeMsr(){
     var olchu= document.getElementById('olchu').value
     document.getElementById('main').style.width=eni+olchu
 }
+function ChngeMsr2(){
+    var hundurluk= document.getElementById('height').value
+    var olchu= document.getElementById('olchu2').value
+    document.getElementById('main').style.height=hundurluk+olchu
+}
+
+function changeColor(){
+    // let color = document.getElementById('colorInputText').value;
+    let color = document.getElementById('colorInputColor').value;
+    document.body.style.backgroundColor =color;
+    document.getElementById('colorInputText').value=color;
+  }
+  
+
+
+
+
+
+// function ChangeColor()
+    // var select = document.querySelector("#my-select");
+    // var body = document.body;
+    // select.addEventListener('change', function() {
+    //   var options = this.options;
+    //   var selectedOption = options[options.selectedIndex];
+    //   body.style.background = selectedOption.value;
+    
+    // });
+
 function Defaultwidth(){
     document.getElementById("main").style.width="100px"
 }
